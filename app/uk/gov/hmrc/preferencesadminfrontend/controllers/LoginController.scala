78cba8e1036b219867befc2f49d4b33c22e8d497
thoeni <thoeni@gmail.com>
2017-03-28 16:47:18 +0100

    DC-888: Implemented sample login page

--- app/uk/gov/hmrc/preferencesadminfrontend/controllers/LoginController.scala
+++ app/uk/gov/hmrc/preferencesadminfrontend/controllers/LoginController.scala
@@ -1,4 +1,4 @@
-@*
+/*
  * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,10 +12,20 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *@
+ */
 
-@()(implicit request: Request[_], messages: Messages))
+package uk.gov.hmrc.preferencesadminfrontend.controllers
 
-@uk.gov.hmrc.preferencesadminfrontend.views.html.main_template(title = "Hello from preferences-admin-frontend", bodyClasses = None) {
-    <h1>Hello from preferences-admin-frontend !</h1>
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+import play.api.mvc._
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+
+import scala.concurrent.Future
+
+
+class LoginController extends FrontendController {
+  val login = Action.async { implicit request =>
+		Future.successful(Ok(uk.gov.hmrc.preferencesadminfrontend.views.html.login()))
+  }
 }
