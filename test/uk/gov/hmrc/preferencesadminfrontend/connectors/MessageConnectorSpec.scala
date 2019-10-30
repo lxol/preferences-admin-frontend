1b7f2cabae32dce3aa8727585c35548663427a03
Alex Olkhovskiy <lxol@users.noreply.github.com>
2019-10-30 10:14:48 +0000

    remove OneAppPerTest

--- test/uk/gov/hmrc/preferencesadminfrontend/connectors/MessageConnectorSpec.scala
+++ test/uk/gov/hmrc/preferencesadminfrontend/connectors/MessageConnectorSpec.scala
@@ -22,10 +22,9 @@ import akka.actor.ActorSystem
 import org.mockito.ArgumentMatchers
 import org.mockito.ArgumentMatchers._
 import org.mockito.Mockito._
-import org.scalatest.TestData
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mockito.MockitoSugar
-import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.http.Status
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json._
@@ -39,12 +38,12 @@ import uk.gov.hmrc.preferencesadminfrontend.model._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class MessageConnectorSpec extends UnitSpec with ScalaFutures with MockitoSugar with GuiceOneAppPerTest {
+class MessageConnectorSpec extends UnitSpec with ScalaFutures with MockitoSugar with GuiceOneAppPerSuite {
     import play.api.inject._
 
     val mockHttp: DefaultHttpClient = mock[DefaultHttpClient]
 
-    override def newAppForTest(testData: TestData) = new GuiceApplicationBuilder().overrides(
+    override lazy val app = new GuiceApplicationBuilder().overrides(
         bind[HttpClient].toInstance(mockHttp)
     ).build()
 
